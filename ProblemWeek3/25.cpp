eb9878f867a9676d4feb7e53f6b9116177cbc847
OAT <64010860@kmitl.ac.th>
2021-08-28 21:08:51 +0700

    Add project files.

---
+++ ProblemWeek3/25.cpp
@@ -0,0 +1,29 @@
+﻿// 25.จงเขียนโปรแกรมรับตัวเลข 1 ตัวแล้วให้คอมพิวเตอร์วาดรูปเครื่องหมาย * เป็นปิระมิดที่มีความสูงเท่ากับตัวเลขที่รับเข้ามา (Level 3)
+
+#include<iostream>
+int main()
+{
+	int a, i, j,k;
+	scanf_s("%d", &a);
+	int total = (a * 2) - 1;
+	int star = a - (a - 1);
+	int blank = a - star;
+
+	for (i = 1; i <= a; i++)
+	{
+		for (j = 1; j <= blank ; j++)
+		{
+			//print blank
+			printf(" ");
+		}
+		for (k = 1; k <= star; k++)
+		{
+			printf("*");
+		}
+		blank -= 1;
+		star += 2;
+		printf("\n");
+	}
+	return 0;
+
+}
